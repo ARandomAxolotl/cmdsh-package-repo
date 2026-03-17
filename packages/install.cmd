13c7a5b3526058bb01e6c437b7641d971caaabe9
unknown <pmminhtriet2013@gmail.com>
2026-03-17 16:06:58 +0700

    cmd went crazy with lf lol

---
+++ packages/install.cmd
@@ -0,0 +1,17 @@
+@echo off
+set "install_path=%1"
+if "%1==" (
+	set "install_path=%CD%"
+	echo Installing to this dir.
+	)
+if not exist cmdSH.cmd (
+	echo Downloaing cmdSH...
+	curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdSH/refs/heads/main/cmdsh.cmd --output cmdSH.cmd
+	echo Configuring cmdSH...
+	call cmdSH.cmd %install_path% --i-want-to-create-config-here --installer
+	)
+if not exist Functions ( mkdir Functions )
+cd Functions
+echo Downloading base packages...
+curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/func.cmd --output func.cmd
+curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/scmdc.cmd --output scmdc.cmd
