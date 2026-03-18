9893206d7580f4a68a920c7cb8eff4f844e086d2
unknown <pmminhtriet2013@gmail.com>
2026-03-18 09:25:43 +0700

    Store cmd file with CRLF in repo

--- packages/install.cmd
+++ packages/install.cmd
@@ -10,12 +10,12 @@ if not exist cmdSH.cmd (
 	echo Configuring cmdSH...
 	call cmdSH.cmd %install_path% --i-want-to-create-config-here --installer
 	)
+(
+	echo [WARNING] : If you have problem with the .cmd files, make sure to open it in notepad++, change the EOL from LF to CRLF.
+	echo Git just too "helpful" to convert them from CRLF to LF
+) > READMEFIRST.txt
 if not exist Functions ( mkdir Functions )
 cd Functions
 echo Downloading base packages...
 curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/func.cmd --output func.cmd
 curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/scmdc.cmd --output scmdc.cmd
-(
-	echo [WARNING] : If you have problem with the .cmd files, make sure to open it in notepad++, change the EOL from LF to CRLF.
-	echo Git just too "helpful" to convert them from CRLF to LF
-) > READMEFIRST.txt
