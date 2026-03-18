b23ff7d5fb858083672b2b49efadc3ff14e67957
unknown <pmminhtriet2013@gmail.com>
2026-03-18 11:05:01 +0700

    idk

--- packages/install.cmd
+++ packages/install.cmd
@@ -1,24 +1,35 @@
 @echo off
-set "install_path=%1"
-if "%1" == "" (
-	set "install_path=."
-	echo Installing to this dir.
-	)
+set "install_path=%~1"
+if "%~1" == "" (
+    set "install_path=."
+    echo Installing to current directory.
+) 
+
 if not exist cmdSH.cmd (
-	echo Downloaing cmdSH...
-	curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdSH/refs/heads/main/cmdsh.cmd --output cmdSH.cmd
-	echo Configuring cmdSH...
-	call cmdSH.cmd %install_path% --i-want-to-create-config-here --installer
-	)
-if not exist Functions ( mkdir Functions )
-cd Functions
+    echo Downloading cmdSH...
+    curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdSH/refs/heads/main/cmdsh.cmd --output cmdSH.cmd
+    echo Configuring cmdSH...
+    call cmdSH.cmd "%install_path%" --i-want-to-create-config-here --installer
+) 
+
+if not exist Functions mkdir Functions
+pushd Functions 
+
 echo Downloading base packages...
-curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/func.cmd --output func.cmd
+curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/main/packages/func.cmd --output func.cmd
 echo Configuring func...
 call func.cmd update
-curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/scmdc.cmd --output scmdc.cmd
-choice /M "Base packages installed, do you want to install other non-essensial packages?"
-if %errorlevel% == 1 ( 
-	call func install man
-	call func install clear
-	) else echo bye!
+curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/main/packages/scmdc.cmd --output scmdc.cmd
+echo Configuring scmdc...
+call scmdc.cmd --installer
+
+choice /M "Base packages installed. Install non-essential packages (man, clear)?"
+if %errorlevel% == 1 (
+    call func install man
+    call func install clear
+) else (
+    echo Skipping optional packages.
+) 
+
+popd
+echo Installation complete!
