d661fe1e931d0f7c6c974dc6197327b4aefdf64d
unknown <pmminhtriet2013@gmail.com>
2026-03-18 10:45:31 +0700

    install.cmd is now better!

--- packages/install.cmd
+++ packages/install.cmd
@@ -1,7 +1,7 @@
 @echo off
 set "install_path=%1"
 if "%1" == "" (
-	set "install_path=%CD%"
+	set "install_path=."
 	echo Installing to this dir.
 	)
 if not exist cmdSH.cmd (
@@ -10,12 +10,15 @@ if not exist cmdSH.cmd (
 	echo Configuring cmdSH...
 	call cmdSH.cmd %install_path% --i-want-to-create-config-here --installer
 	)
-(
-	echo [WARNING] : If you have problem with the .cmd files, make sure to open it in notepad++, change the EOL from LF to CRLF.
-	echo Git just too "helpful" to convert them from CRLF to LF
-) > READMEFIRST.txt
 if not exist Functions ( mkdir Functions )
 cd Functions
 echo Downloading base packages...
 curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/func.cmd --output func.cmd
+echo Configuring func...
+call func.cmd update
 curl -sL https://raw.githubusercontent.com/ARandomAxolotl/cmdsh-package-repo/refs/heads/main/packages/scmdc.cmd --output scmdc.cmd
+choice /M "Base packages installed, do you want to install other non-essensial packages?"
+if %errorlevel% == 1 ( 
+	call func install man
+	call func install clear
+	) else echo bye!
