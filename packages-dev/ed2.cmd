d5e7dd94c1f4819090dfb2c1d761f9525a4bfc13
Triet <pmminhtriet2013@gmail.com>
2026-07-10 19:42:20 +0700

    Revert "insert mode added, kinda..working i guess"
    This reverts commit c5b6265d7f7b80ee9a622fdd77ba82dac0808ee4.

--- packages-dev/ed2.cmd
+++ packages-dev/ed2.cmd
@@ -21,23 +21,22 @@ set "tmpfile=%sessionid%_%outputfile%.tmp"
 echo %sessionid%>"%lckfile%"
 
 if not exist "%sessionconf%" echo %outputfile%>"%sessionconf%"
-if exist "%outputfile%" ( 
+if exist "%outputfile% ( 
   type "%outputfile%">"%tmpfile%"
 ) else (
   type nul>"%tmpfile%"
 )
 
 :loop
-set "errlv=1"
+:: setlocal Disabledelayedexpandsion
 set "i="
 set /p "i=(%outputfile%): "
 
 if "%i%"=="a" goto :append
-if "%i%"=="i" goto :insert
 :: append
 if "%i%"=="w" goto :write
 :: write, yes
-if "%i%"=="e" goto :openmorefile
+if "%i%"=="e" goto :exit
 if "%i%"=="q" goto :exit
 :: quit
 if "%i%"=="rf" goto :readfile
@@ -45,9 +44,9 @@ if "%i%"=="rf" goto :readfile
 if "%i%"=="rt" goto :readtmpbuffer
 :: read tmpbuffer
 
-if "%i%"=="ct" type nul>"%tmpfile%" & set errlv=0
+if "%i%"=="dt" type nul>"%tmpfile%"
 :: clear tmpbuffer
-if "%i%"=="cf" type nul>"%outputfile%" & set errlv=0
+if "%i%"=="df" type nul>"%outputfile%"
 :: clear file!!
 
 if "%i%"=="fo" goto :openmorefile
@@ -59,25 +58,18 @@ if "%i%"=="fl" type "%sessionconf%"
 if "%i%"=="h"  set help=1 && goto :helpcommon
 if "%i%"=="ha" goto :help
 if "%i%"=="hf" set help=1 && goto :helpfile
-if "%i%"=="hd" set help=1 && goto :helpdel
-if "%i%"=="hr" set help=1 && goto :helpread
+if "%i%"=="hd" set help=1 && goto :helpdel -a
+if "%i%"=="hr" set help=1 && goto :helpread -a
 :: help!
-set help=0
-if "errlv"=="1" echo Unknown command: '%i%'. Type 'h' for help.
+echo Unknown command: '%i%'. Type 'h' for help.
 
 goto :loop
 
 :append
-setlocal EnableDelayedExpansion 
 set "i="
 set /p "i=(appending %outputfile%): "
-if "!i:~0,1!"=="." (
-  endlocal
-  goto :loop
-)
-(
-  echo(!i!
-)>>"%tmpfile%"
+if "%i%"=="." goto :loop
+echo %i%>>"%tmpfile%"
 goto :append
 
 :write
@@ -139,16 +131,16 @@ set conf=%~2
 set /a i=0
 
 for /f "delims=" %%A in ("%conf%") do (
-  set /a i+=1
-  if !i! equ %n% (
-    set "line=%%A"
-    goto :found
-  )
+    set /a i+=1
+    if !i! equ %n% (
+        set "line=%%A"
+        goto :found
+    )
 )
 :found
 endlocal && set "outputfile=%line%"
 set /a openingfileid=(openingfileid%%filecount)+1
-goto :changefileprepare
+goto :lckfileprepare
 
 :cleanup
 setlocal EnableDelayedExpansion
@@ -166,112 +158,37 @@ exit /b
 :help
 :helpcommon
 echo Edit commands : 
-echo 'a'  : append text
-echo 'i'  : insert text
+echo 'a'     : append text
 echo.
 echo Exit :
-echo 'q'  : quit, discarding any changes
+echo 'q','e' : quit
 echo.
 echo Write :
-echo 'w'  : write buffer to disk
+echo 'w'     : write buffer to disk
 echo.
 echo Help :
-echo 'h'  : general help
-echo 'ha' : show all help
-echo 'hr' : read commands help
-echo 'hd' : delete/clear commands help
-echo 'hf' : file commands help
-echo.
-echo Alias :
-echo 'e'  = 'fo'
+echo 'h'     : general help
+echo 'ha'    : show all help
+echo 'hr'    : read commands help
+echo 'hd'    : delete/clear commands help
+echo 'hf'    : file commands help
 echo.
 if "%help%"=="1" goto :loop
 :helpread
 echo Read commands :
-echo 'rt' : read write buffer
-echo 'rf' : read file from disk
+echo 'rt'    : read write buffer
+echo 'rf'    : read file from disk
 echo.
 if "%help%"=="1" goto :loop
 :helpdel
 echo Clear/Delete commands : 
-echo 'ct' : clear write buffer
-echo 'cf' : clear file on disk(caution!)
+echo 'dt'    : clear write buffer
+echo 'df'    : clear file on disk(caution!)
 echo.
 if "%help%"=="1" goto :loop
 :helpfile
 echo Files command :
-echo 'fo' : open/create a file
-echo 'fc' : cycle through open files
-echo 'fl' : list open files.
+echo 'fo'    : open/create a file
+echo 'fc'    : cycle through open files
+echo 'fl'    : list open files.
 goto :loop
-
-:insert
-set "i="
-set /p "i=Line :"
-if "%i%"=="" ( 
-  echo Input a line. 
-  goto :loop
-)
-set "insertline=%i%"
-call :insertprepare "%insertline%" "%tmpfile%"
-goto :doinsert
-:finishinsert
-call :cleaninsert "%insertline%" "%tmpfile%"
-type "%inserttmpbuffer%">"%tmpfile%"
-del "%inserttmpbuffer%"
-goto :loop
-
-:changefileprepare
-set "lckfile=%outputfile%.lck"
-goto :prepare
-
-:insertprepare
-setlocal EnableDelayedExpansion
-
-set "source=%~2"
-set "target=%~2.2"
-set "limit=%~1"
-
-(
-    set /a count=0
-    for /f "delims=" %%A in ('type "%source%"') do (
-        set /a count+=1
-        if !count! LSS %limit% echo %%A
-    )
-) > "%target%"
-
-endlocal && set "inserttmpbuffer=%target%"
-exit /b
-
-:doinsert
-setlocal EnableDelayedExpansion && set "inserttmpbuffer=%inserttmpbuffer%" && set "crrinsertline=%insertline%"
-set "i="
-set /p "i=(inserting %outputfile% at line %crrinsertline%): "
-set /a crrinsertline+=1
-if "!i:~0,1!"=="." (
-  endlocal
-  goto :finishinsert
-)
-(
-  echo(!i!
-)>>"%inserttmpbuffer%"
-goto :doinsert
-
-:cleaninsert
-setlocal EnableDelayedExpansion
-
-set "source=%~2"
-set "target=%~2.2"
-set "limit=%~1"
-
-(
-  set /a count=0
-  for /f "delims=" %%A in ('type "%source%"') do (
-    set /a count+=1
-    if !count! GTR %limit% (
-      >>"%target%" echo(%%A
-    )
-  )
-)
-endlocal
-exit /b
